b547203ec83689f041a0eefd7b4ae83a5589b02c
aditya-tripathee <adityatripatheee@gmail.com>
2026-03-10 18:39:02 +0530

    updated

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -15,7 +15,8 @@ const Experience = () => {
             <h2 className="text-2xl font-bold text-gray-900">Ridezzup.com</h2>
 
             <div className="flex flex-wrap gap-3 mt-3 md:mt-0">
-              <span className="px-4 py-1 bg-blue-600 text-white rounded-2xl text-xs font-semibold">
+              <span className="px-5 py-2 rounded-xl bg-gradient-to-r from-blue-500 to-purple-600 text-white font-medium shadow-lg
+                                   hover:scale-105 hover:shadow-2xl transition transform duration-300">
                 Frontend Web Developer Intern
               </span>
               <span className="px-4 py-1 bg-gray-900 text-gray-200 rounded-2xl text-xs font-medium">
