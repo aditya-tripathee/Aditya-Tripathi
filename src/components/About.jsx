c915d26274db1e98a065caf15ea010d9fbaa40eb
Aditya Tripathi <145194957+aditya-tripathee@users.noreply.github.com>
2026-03-20 14:13:52 +0530

    Update About component link to new URL

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -67,7 +67,7 @@ const About = () => {
 
           <div className="mt-5">
             <a
-              href="https://drive.google.com/file/d/1NKtkxphGy7V-3_pau65wGmo8Vjx25vAq/view?usp=drive_link"
+              href="https://drive.google.com/file/d/13Cb8jfheLbOOSUw03O-mOdJQUr5Yv-Vs/view?usp=drive_link"
               target="_blank"
               rel="noopener noreferrer"
             >
