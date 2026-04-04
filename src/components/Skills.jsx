b6355c2f5cf9516a90b55a22b3005141ff2fe48c
Aditya Tripathi <145194957+aditya-tripathee@users.noreply.github.com>
2026-04-04 14:45:43 +0530

    Add PostgreSQL to tools list in Skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,7 +4,7 @@ const languages = ["JavaScript","C++","Typescript"];
 const webs = ["ReactJS","NodeJS","ExpressJS","HTML5","CSS3","MongoDB","Tailwind CSS","MySQL"];
 const frontend = ["HTML5","CSS3","Tailwind CSS","Prebuilt UI","Framer Motion","ShadCN","React.js","Next.js","Redux Toolkit"];
 const backend = ["Node.js","Express.js","REST APIs","JWT Authentication","Mongoose"];
-const tools = ["MySQL","MongoDB","Git","Github","Postman","Docker","Vercel"];
+const tools = ["MySQL","MongoDB","PostgreSQL","Git","Github","Postman","Docker","Vercel"];
 
 const Skills = () => {
   return (
